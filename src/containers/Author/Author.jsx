6e4f6de31c9c0d68f3b5ef931023d66538b7ed72
Artyom Resh <artyomresh@gmail.com>
2017-02-20 17:05:53 +0300

    add back link to home

--- src/containers/Author/Author.jsx
+++ src/containers/Author/Author.jsx
@@ -1,11 +1,13 @@
 import React, { Component} from 'react';
 import styles from './Author.styl';
+import { Link } from 'react-router';
 
 class Author extends Component {
   render() {
     return (
       <div className={styles.author}>
         <h1>Artyom Resh</h1>
+        <Link to={'/'} className={styles.link}>Back to home</Link>
       </div>
     );
   }
